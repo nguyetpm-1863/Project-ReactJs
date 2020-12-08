795a284294a9f28b0bfa1313224487b5bb626194
nguyetpm-1863 <pham.thi.minh.nguyet@sun-asterisk.com>
2020-12-08 15:49:54 +0700

    Updates

---
+++ precache-manifest.2f02cd758d804cae3e6b2063aa72955c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2dbfa4f531d88278875acfc405e57d1",
+    "url": "/Project-ReactJs/index.html"
+  },
+  {
+    "revision": "ef94374088647f2ad432",
+    "url": "/Project-ReactJs/static/css/2.907ae24a.chunk.css"
+  },
+  {
+    "revision": "e3a0874f826ad5cb77a5",
+    "url": "/Project-ReactJs/static/css/main.07d858cb.chunk.css"
+  },
+  {
+    "revision": "ef94374088647f2ad432",
+    "url": "/Project-ReactJs/static/js/2.98eec996.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Project-ReactJs/static/js/2.98eec996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3a0874f826ad5cb77a5",
+    "url": "/Project-ReactJs/static/js/main.89058dd2.chunk.js"
+  },
+  {
+    "revision": "c4906f7873ef73eeb256",
+    "url": "/Project-ReactJs/static/js/runtime-main.dd35fa8a.js"
+  },
+  {
+    "revision": "037d80ac5b173da9d332f868e0ba62c1",
+    "url": "/Project-ReactJs/static/media/banner-collection.037d80ac.jpg"
+  },
+  {
+    "revision": "c091a3783e4c11c7f1a2ab9ecb05e740",
+    "url": "/Project-ReactJs/static/media/banner.c091a378.jpg"
+  },
+  {
+    "revision": "4142079dfa5c5d52ac7b5c52a17d2822",
+    "url": "/Project-ReactJs/static/media/cart.4142079d.svg"
+  },
+  {
+    "revision": "44d36250b945776cfa22d96dfb61bcc2",
+    "url": "/Project-ReactJs/static/media/logo.44d36250.png"
+  },
+  {
+    "revision": "f3619325691aff5533d1e426740412b6",
+    "url": "/Project-ReactJs/static/media/slider-1.f3619325.jpg"
+  },
+  {
+    "revision": "3c1ab38c4c03d5b0cd43c53e3b5537ce",
+    "url": "/Project-ReactJs/static/media/slider-2.3c1ab38c.jpg"
+  },
+  {
+    "revision": "22d21235b0d3cb5f608bb7201d849544",
+    "url": "/Project-ReactJs/static/media/slider-3.22d21235.jpg"
+  },
+  {
+    "revision": "e5ab5d3bfa98bbb77d41919bd66b5977",
+    "url": "/Project-ReactJs/static/media/slider-4.e5ab5d3b.jpg"
+  }
+]);
